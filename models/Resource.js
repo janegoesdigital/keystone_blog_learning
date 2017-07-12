377e67cc0c5ed26cf70a65db0caaa556fe73578f
jadarling <janeodarling@gmail.com>
2017-07-13 01:21:34 +1000

    amend the data model for knowledge bank

--- models/Resource.js
+++ models/Resource.js
@@ -13,17 +13,19 @@ var Resource = new keystone.List('Resource', {
 
 Resource.add({
 	title: { type: String, required: true },
-	link: { type: Types.Url},
-	// document: { type: Types.AzureFile },
+	link: { type: Types.Url}
+
 	status: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	author: { type: Types.Relationship, ref: 'User', index: true },
+	contributor: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-	// image: { type: Types.CloudinaryImage },
 	content: {
 		description: { type: Types.Html, wysiwyg: true, height: 150 },
 		// extended: { type: Types.Html, wysiwyg: true, height: 400 },
 	},
+	//I want to use the same set of topic categories as I use for posts - so that on the bottom of a post page I can link automatically to any resources I have on the topic
 	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+	//Set of categories for the type of content it might be.
+	contentType: {type: Types.Select, options: 'cool tool, good blog, interesting article, interesting news', index: true},
 });
 
 Resource.schema.virtual('content.full').get(function () {
