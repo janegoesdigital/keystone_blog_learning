5024941387ebd8947e68a100aba9693ffc0a0706
jadarling <janeodarling@gmail.com>
2017-10-19 00:31:40 +1100

    Added details to the knowledge bank to use it for research

--- models/Resource.js
+++ models/Resource.js
@@ -13,24 +13,40 @@ var Resource = new keystone.List('Resource', {
 
 Resource.add({
 	title: { type: String, required: true },
-	link: { type: Types.Url}
+	link: { type: Types.Url},
+	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+	addedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 
-	status: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-	contributor: { type: Types.Relationship, ref: 'User', index: true },
-	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+	contentType: {type: Types.Select, options: 'entity, reading', index: true},
+	entityType: {type: Types.Select, options: 'person, uni, gov body,research centre', index: true, dependsOn: {contentType: 'entity'} },
+	status: {type: Types.Select, options: 'read, toRead', default: 'toRead', dependsOn: { contentType: 'reading'}},
+	readingType : {type: Types.Select, options: 'book, article, report', dependsOn: { contentType: 'reading'}, },
+	publicationYear: { type: Types.Date, index: true, dependsOn: { contentType: 'reading'} },
+	author: {type:Types.Text, dependsOn: {contentType: 'reading'}},
+	rating: {type: Types.Select, options: 'annoying, ok, good, I love it',},
+	location: {Type: Types.Text},
+
+
+
+
+	// status: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+	// contributor: { type: Types.Relationship, ref: 'User', index: true },
+
+
+	citation: { type: Types.Text, dependsOn: { readingType: 'article'} },
+	publisher: { type: Types.Text, dependsOn: { readingType: 'book'} },
 	content: {
 		description: { type: Types.Html, wysiwyg: true, height: 150 },
-		// extended: { type: Types.Html, wysiwyg: true, height: 400 },
+		extended: { type: Types.Html, wysiwyg: true, height: 400 },
 	},
 	//I want to use the same set of topic categories as I use for posts - so that on the bottom of a post page I can link automatically to any resources I have on the topic
-	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
-	//Set of categories for the type of content it might be.
-	contentType: {type: Types.Select, options: 'cool tool, good blog, interesting article, interesting news', index: true},
+
+
 });
 
 Resource.schema.virtual('content.full').get(function () {
 	return this.content.extended || this.content.brief;
 });
 
-Resource.defaultColumns = 'title, status|20%,link|20%, content.description|20%, categories|20%';
+Resource.defaultColumns = 'title, contentType|10%, link|20%, content.description|10%, content.extended|20%, categories|20%';
 Resource.register();
